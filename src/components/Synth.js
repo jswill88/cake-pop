a86304746af5017ea1127f0ed54aa9087f10a7a3
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-01-11 00:13:04 -0800

    make 16 beats with a full scale and reset

--- src/components/Synth.js
+++ src/components/Synth.js
@@ -1,33 +1,56 @@
 import { useEffect, useState, useRef } from 'react';
 import * as Tone from 'tone';
 
+// const audioC ontext = new AudioContext()
+
 export default function Synth() {
   const [showDiv, setShowDiv] = useState(false)
-  const [started, setStarted] = useState(false)
-  const [loop1Arr, setLoop1Arr] = useState([])
+  // const [started, setStarted] = useState(false)
+  const [loop1Arr, setLoop1Arr] = useState({});
   const noteToAdd = useRef('')
 
   const startAudio = async () => {
     await Tone.start();
-    Tone.setContext(new Tone.Context({ latencyHint : "playback" }));
-    setStarted(true)
     Tone.Transport.start('+0.1');
   }
-  useEffect(() => {
-    const synth = makeSynth();
-    const synth2 = new Tone.FMSynth().toDestination();
-    const loop = new Tone.Sequence((time, note) => {
-      synth.triggerAttackRelease(note, '16n', time);
-    }, loop1Arr).start(0);
-    const loop2 = new Tone.Sequence((time, note) => {
-      synth2.triggerAttackRelease(note, '16n', time);
-    }, ['F3', [],'F3',[]]).start(0);
-    return () => {
-      loop.cancel()
-      loop2.cancel();
-    }
 
-  }, [started, loop1Arr])
+  const reset = () => {
+    for (let loop in loop1Arr) {
+      for (let i = 0; i < 16; i++) {
+        if (loop1Arr[loop][i]) {
+          loop1Arr[loop][i].stop()
+        }
+      }
+    }
+    const noteObj = { Ab5: {}, G5: {}, F5: {}, Eb5: {}, Db5: {}, C5: {}, Bb4: {}, Ab4: {} }
+    for (let note in noteObj) {
+      for (let i = 0; i < 16; i++) noteObj[note][i] = false;
+    }
+    setLoop1Arr(noteObj)
+
+  }
+  useEffect(() => {
+    reset();
+  },[])
+
+  const addSynth = (beat, note) => {
+    if (!loop1Arr[note][beat]) {
+      const arrLoop = new Array(16).fill([])
+      arrLoop[beat] = note;
+      console.log(arrLoop)
+      console.log('in add synth')
+      const synth = makeSynth();
+      const loop = new Tone.Sequence((time, note) => {
+        console.log('time', time)
+        synth.triggerAttackRelease(note, '16n', time);
+      }, arrLoop).start(0);
+      setLoop1Arr(obj => ({ ...obj, [note]: { ...obj[note], [beat]: loop } }));
+    } else {
+      // console.log
+      loop1Arr[note][beat].stop();
+      setLoop1Arr(obj => ({ ...obj, [note]: { ...obj[note], [beat]: false } }));
+    }
+  }
 
   return (
     <>
@@ -36,39 +59,58 @@ export default function Synth() {
           startAudio()
           setShowDiv(true)
         }}
-      >Synth</h1>
+        style={{ border: '2px solid black', width: '100px' }}
+      >Start</h1>
       <input
-      type="text"
-      onChange={(e) => noteToAdd.current = e.target.value}
+        type="text"
+        onChange={(e) => noteToAdd.current = e.target.value}
       />
-      <button
-      onClick={() => {
-        console.log(noteToAdd.current)
-        setLoop1Arr(arr => [...arr, noteToAdd.current])}}
-      ></button>
+      {Object.keys(loop1Arr).map(note =>
+        <div
+          key={note}
+        >
+          {Object.keys(loop1Arr[note]).map(beat =>
+            <button
+            key={beat}
+              style={{
+                backgroundColor: loop1Arr[note][beat] === false ? 'pink' : 'lightblue',
+                width: '40px'
+              }}
+              onClick={() => addSynth(beat, note)}
+            >{note}</button>
+          )}
+        </div>
+      )}
+
+
       {showDiv && <div
-      onClick={() => {
-        Tone.Transport.stop()
-        setShowDiv(false);
-      }}
-      >Click Work</div>}
+        onClick={() => {
+          Tone.Transport.stop()
+          setShowDiv(false);
+        }}
+        style={{ border: '3px dotted red', width: 100 }}
+      >Stop</div>}
+
+      <button
+        onClick={() => reset()}
+      >Reset</button>
     </>
   )
 }
 
 function makeSynth() {
   return new Tone.Synth({
-    "volume": 0,
-    "detune": 0,
-    "portamento": 0.05,
-    "envelope": {
-      "attack": 0.05,
-      "attackCurve": "exponential",
-      "decay": 0.2,
-      "decayCurve": "exponential",
-      "release": 1.5,
-      "releaseCurve": "exponential",
-      "sustain": 0.2
+    volume: 0,
+    detune: 0,
+    portamento: 0.05,
+    envelope: {
+      attack: 0.05,
+      attackCurve: "exponential",
+      decay: 0.2,
+      decayCurve: "exponential",
+      release: 1.5,
+      releaseCurve: "exponential",
+      sustain: 0.2
     },
     "oscillator": {
       "partialCount": 0,
