c4f5eb8c8d423e6dcfea8f7fe57208239f3ff89a
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-01-15 00:06:31 -0800

    add ability to change chords each measure

--- src/components/Synth.js
+++ src/components/Synth.js
@@ -1,12 +1,35 @@
 import { useEffect, useState } from 'react';
 import * as Tone from 'tone';
+import he from 'he';
 
 
 export default function Synth() {
-  const [loop1Arr, setLoop1Arr] = useState({});
-  const [currentBeat, setCurrentBeat] = useState(-1)
+  const [noteSwitches, setNoteSwitches] = useState({});
+  const [currentBeat, setCurrentBeat] = useState(-1);
+  const [prog, setProg] = useState(['I', 'V', 'vi', 'IV'])
 
   const loopLength = 32;
+  const chords = {
+    I: ['Ab4', 'C5', 'Eb5'],
+    ii: ['Bb4', 'Db5', 'F5'],
+    iii: ['G4', 'C5', 'Eb5'],
+    IV: ['Ab4', 'Db5', 'F5'],
+    V: ['G4', 'Bb4', 'Eb5'],
+    vi: ['Ab4', 'C5', 'F5'],
+    'vii&#x26AC;': ['G5', 'Bb4', 'Db5'],
+  }
+
+  const notes = {
+    low: [chords[prog[0]][0], chords[prog[1]][0], chords[prog[2]][0], chords[prog[3]][0]],
+    mid: [chords[prog[0]][1], chords[prog[1]][1], chords[prog[2]][1], chords[prog[3]][1]],
+    high: [chords[prog[0]][2], chords[prog[1]][2], chords[prog[2]][2], chords[prog[3]][2]]
+  }
+
+  useEffect(() => {
+    console.log(prog)
+  }, [prog])
+
+
 
   const startAudio = async () => {
     setCurrentBeat(-1)
@@ -15,28 +38,28 @@ export default function Synth() {
   }
 
   const reset = () => {
-    for (let loop in loop1Arr) {
+    for (let loop in noteSwitches) {
       for (let i = 0; i < loopLength; i++) {
-        if (loop1Arr[loop][i]) {
-          loop1Arr[loop][i].stop()
+        if (noteSwitches[loop][i]) {
+          noteSwitches[loop][i].stop()
         }
       }
     }
     Tone.Transport.stop();
-    const noteObj = { Ab5: {}, G5: {}, F5: {}, Eb5: {}, Db5: {}, C5: {}, Bb4: {}, Ab4: {} }
+    const noteObj = { high: {}, mid: {}, low: {} }
     for (let note in noteObj) {
       for (let i = 0; i < loopLength; i++) noteObj[note][i] = false;
     }
-    setLoop1Arr(noteObj)
-    setCurrentBeat(-1)
+    setNoteSwitches(noteObj)
+    setCurrentBeat(-2)
   }
 
   useEffect(() => {
-    const noteObj = { Ab5: {}, G5: {}, F5: {}, Eb5: {}, Db5: {}, C5: {}, Bb4: {}, Ab4: {} }
+    const noteObj = { high: {}, mid: {}, low: {} }
     for (let note in noteObj) {
       for (let i = 0; i < loopLength; i++) noteObj[note][i] = false;
     }
-    setLoop1Arr(noteObj)
+    setNoteSwitches(noteObj)
 
     const loop = new Tone.Loop(time => {
       Tone.Draw.schedule(() => {
@@ -47,18 +70,18 @@ export default function Synth() {
   }, [])
 
 
-  const addSynth = (beat, note) => {
-    if (!loop1Arr[note][beat]) {
+  const addSynth = (beat, note, row) => {
+    if (!noteSwitches[row][beat]) {
       const arrLoop = new Array(loopLength).fill([])
       arrLoop[beat] = note;
       const synth = makeSynth();
       const loop = new Tone.Sequence((time, note) => {
         synth.triggerAttackRelease(note, '8n', time);
       }, arrLoop).start(0);
-      setLoop1Arr(obj => ({ ...obj, [note]: { ...obj[note], [beat]: loop } }));
+      setNoteSwitches(obj => ({ ...obj, [row]: { ...obj[row], [beat]: loop } }));
     } else {
-      loop1Arr[note][beat].stop();
-      setLoop1Arr(obj => ({ ...obj, [note]: { ...obj[note], [beat]: false } }));
+      noteSwitches[row][beat].stop();
+      setNoteSwitches(obj => ({ ...obj, [row]: { ...obj[row], [beat]: false } }));
     }
   }
 
@@ -87,42 +110,69 @@ export default function Synth() {
           style={{ border: '2px solid black', width: 100, color: 'red', textAlign: 'center' }}
         >Stop</h1>
       </section>
-      {Object.keys(loop1Arr).map(note =>
-        <div
-          key={note}
-          style={{
-            width: '95%',
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          {Object.keys(loop1Arr[note]).map((beat, i) =>
-            <span
-              key={beat}
-              style={{
-                boxSizing: 'border-box',
-                backgroundColor: loop1Arr[note][beat] === false ? 'pink'
-                  : i === currentBeat ? 'yellow' : 'lightblue',
-                border: i === currentBeat ? '2px solid black' : 'none',
-                width: '30px',
-                height: '30px',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                margin: '2px',
-                borderRadius:'50%',
-                fontSize: '.9em'
-              }}
-              onClick={() => addSynth(beat, note)}
-            >{note.slice(0, note.length - 1)}
-            </span>
-          )}
-        </div>
-      )}
+
+      <div style={{ display: 'flex' }}>
+        {prog.map((progChord, i) =>
+          <select
+            key={i}
+            defaultValue={he.decode(progChord)}
+            onChangeCapture={e => {
+              setProg(arr => {
+                const arrCopy = [...arr]
+                console.log(he.encode(e.target.value))
+                arrCopy[i] = he.encode(e.target.value);
+                return arrCopy;
+              })
+            }}>
+            {Object.keys(chords).map((chord, j) =>
+              <option
+                key={j}
+                
+              >{he.decode(chord)}</option>
+            )}
+          </select>
+        )}
+      </div>
+
+      {
+        Object.keys(noteSwitches).map(noteRow =>
+          <div
+            key={noteRow}
+            style={{
+              width: '95%',
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            {Object.keys(noteSwitches[noteRow]).map((beat, i) =>
+              <span
+                key={beat}
+                style={{
+                  boxSizing: 'border-box',
+                  backgroundColor: noteSwitches[noteRow][beat] === false ? 'pink'
+                    : i === currentBeat ? 'yellow' : 'lightblue',
+                  border: i === currentBeat ? '2px solid black' : 'none',
+                  width: '30px',
+                  height: '30px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  margin: '2px',
+                  borderRadius: '50%',
+                  fontSize: '.9em'
+                }}
+                onClick={() => addSynth(beat, notes[noteRow][Math.floor(i / loopLength * 4)], noteRow)}
+              >
+                {notes[noteRow][Math.floor(i / loopLength * 4)].slice(0, notes[noteRow][Math.floor(i / loopLength * 4)].length - 1)}
+              </span>
+            )}
+          </div>
+        )
+      }
       <button
         onClick={() => reset()}
       >Reset</button>
-    </div>
+    </div >
   )
 }
 
