a426adb1e44bc172b229299a137b52470638d7f2
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-02-26 18:06:13 -0800

    Add li and gh tags to footer

--- src/components/Foot.js
+++ src/components/Foot.js
@@ -1,8 +1,11 @@
 
+import GithubOutlined from '@ant-design/icons/GithubOutlined'
+import LinkedinOutlined from '@ant-design/icons/LinkedinOutlined'
 
+import Tag from 'antd/es/tag'
 import Typography from 'antd/es/typography';
 
-const { Text } = Typography;
+const { Text, Link } = Typography;
 
 export default function Foot() {
 
@@ -11,13 +14,33 @@ export default function Foot() {
       <Text
         style={{ color: '#ffffff' }}
       >
-        Josh Williams &copy; 2021 
+        Josh Williams &copy; 2021
     </Text>
       <Text
         style={{ color: '#ffffff' }}
       >
-        GitHub, LinkedIn, Portfolio, Polyrhythm generator
+        {' '}Portfolio, Polyrhythm Generator{' '}
       </Text>
+      <Link
+        href="https://github.com/jswill88"
+      >
+        <Tag
+          icon={<GithubOutlined />}
+          color="purple"
+        >
+          GitHub
+        </Tag>
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/joshua-s-williams/"
+      >
+        <Tag
+          icon={<LinkedinOutlined />}
+          color="blue"
+        >
+          LinkedIn
+        </Tag>
+      </Link>
     </>
   )
 }
